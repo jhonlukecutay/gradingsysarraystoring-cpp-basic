1e8abcf6cd9bf284e8d1528f5930494598cb2935
Jhon Luke Cutay <113082100+jhonlukecutay@users.noreply.github.com>
2022-09-28 16:32:09 +0800

    Update gradingsys(arraystoring).cpp

--- gradingsys(arraystoring).cpp
+++ gradingsys(arraystoring).cpp
@@ -9,20 +9,14 @@ int main() {
     cout<<"Grading System (w/ ArrayStoring)"<<endl<<endl<<"Please enter the name of student: ";
     getline(cin, nameStu); name = nameStu;
     cout<<endl<<"How many grades are need to be calculated? Input here: "; cin>>gradesLimit;
-    cout<<endl<<"The grades that need to be calculated are: "<<gradesLimit<<endl<<endl;
+    cout<<endl<<"The number of grades that needed to be calculated are: "<<gradesLimit<<endl<<endl;
 
     for(gradesCal=1;gradesCal<=gradesLimit;gradesCal++) {
         cout<<"Input the grades: "; cin>>grade[gradesLimit];
         totalg = totalg + grade[gradesLimit];
 }
     average = totalg / gradesLimit;
-    cout<<endl<<endl<<"The Student Name is "<<name<<" and his/her general average is: "<<average<<endl<<endl;
+    cout<<endl<<endl<<"The student's name is "<<name<<" and his/her general average is: "<<average<<endl;
     result = (average > 75) ? "The student passed the semester." : "The student failed to pass the semester.";
-    cout<<endl<<endl<<result<<endl<<endl;
-
-
-
-
-
-
+    cout<<endl<<result<<endl<<endl;
 }
